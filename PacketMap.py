25adcadd0e89630068a87599bf4efcbcde97a3cd
Bad-Relay <36608560+Bad-Relay@users.noreply.github.com>
2018-02-18 17:22:11 -0700

    Packet Data Read
    Packet data can now be read in a normal format

--- PacketMap.py
+++ PacketMap.py
@@ -3,12 +3,46 @@ import struct
 import textwrap
 import os
 from Tkinter import *
+from ctypes import *
 #computer host
 root = Tk()
 
 #Defult Ip
 host = "192.168.99.149"
 
+#Ip header table
+class IP(Structure):
+    _fields_ = [
+        ("ihl", c_ubyte, 4),
+        ("version", c_ubyte, 4),
+        ("tos", c_ubyte),
+        ("len", c_ushort),
+        ("id", c_ushort),
+        ("offset", c_ushort),
+        ("ttl", c_ubyte),
+        ("protocol_num", c_ubyte),
+        ("sum", c_ushort),
+        ("src", c_ulong),
+        ("dst", c_ulong)
+    ]
+
+    def __new__(self, socket_buffer=None):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+
+        # map protocol constants to their names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+
+        # human readable IP addresses
+        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+
+        # human readable protocol
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
 
 def capture_packet():
 
@@ -32,11 +66,17 @@ def capture_packet():
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
     #set var for gui
     packet = StringVar()
-    packet.set(sniffer.recvfrom(65565))
-    #print in consol
-    print sniffer.recvfrom(65565)
+    raw_buffer = sniffer.recvfrom(65565)[0]
+    ip_header = IP(raw_buffer[0:20])
+    packet.set("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+    #print in console
+    raw_buffer = sniffer.recvfrom(65565)[0]
 
-    #set packet
+    # create an IP header from the first 20 bytes of the buffer
+    ip_header = IP(raw_buffer[0:20])
+
+    print "Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
+    #set packet gui
     label = Label(root, text= packet.get())
     label.pack()
     #promiscuous mode off
